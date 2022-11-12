8f6cefdc575618d1e6fc955207edc052277442c5
pritthwiraz <pritthwiraz@gmail.com>
2022-11-12 21:00:28 +0600

    Updated project

--- src/Pages/ServiceDetails/Review.js
+++ src/Pages/ServiceDetails/Review.js
@@ -4,10 +4,10 @@ const Review = () => {
     return (
         <form className="form-control my-8">
             <label className="label">
-                <span className="label-text">Add Your Review</span>
-                <span className="label-text-alt">Alt label</span>
+                <span className="label-text text-2xl">Add Your Review</span>
+                
             </label>
-            <textarea className="textarea textarea-bordered h-24" placeholder="Review"></textarea>
+            <textarea className="textarea textarea-bordered h-24 text-xl" placeholder="Review"></textarea>
             <button className='btn btn-primary my-3 w-2/6'>Submit your Review</button>
         </form>
     );
