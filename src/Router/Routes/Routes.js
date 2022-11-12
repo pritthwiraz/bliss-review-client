9a99122183e9c65b9850a9ee056ed6b2a13f75ca
pritthwiraz <pritthwiraz@gmail.com>
2022-11-12 21:56:28 +0600

    firebase

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,6 +1,7 @@
 import Main from "../../Layout/Main";
 import Blog from "../../Pages/Blog/Blog";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
 import ServiceDetails from "../../Pages/ServiceDetails/ServiceDetails";
 
 const { createBrowserRouter } = require("react-router-dom");
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
                 path: '/servicedetails/:id',
                 element: <ServiceDetails></ServiceDetails>,
                 loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             }
         ]
     }
